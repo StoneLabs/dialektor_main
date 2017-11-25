75a485a1a3fc4d8984888c8ce684b8aa8e5b4394
Stone Labs <levyehrstein@googlemail.com>
2017-11-25 11:46:51 +0100

    Working wav input; Debug output; Added new dependencies

--- INSTALLATION.md
+++ INSTALLATION.md
@@ -1,5 +1,8 @@
 #Install Accord.NET
 First make sure to install Accord.NET using package manager of your choise.
 The easiest way to get started is through NuGet:
+
 	PM> Install-Package Accord.MachineLearning
     PM> Install-Package Accord.Controls
+	PM> Install-Package Accord.Audio
+	PM> Install-Package Accord.DirectSound
--- Program.cs
+++ Program.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Accord;
+using Accord.Audio;
+using Accord.Audio.Formats;
+using Accord.DirectSound;
+using System.Windows.Forms;
 
 namespace dialektor
 {
@@ -11,8 +14,29 @@ namespace dialektor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi, world!");
-            Console.ReadKey(true);
+            if (Environment.OSVersion.Version.Major >= 6)
+                SetProcessDPIAware();
+
+            WaveFileAudioSource source = new Accord.DirectSound.WaveFileAudioSource("test_sound.wav");
+
+            // Specify the callback function which will be
+            // called once a sample is completely available
+            source.NewFrame += source_NewFrame;
+
+            source.Start();
+            while (true) { Console.ReadKey(true); }
         }
+
+        private static async void source_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            // Read current frame...
+            Signal signal = eventArgs.Signal;
+
+            String data = String.Join(",", signal.RawData.Select(p => p.ToString()).ToArray());
+            Console.WriteLine("Loaded signal: [" + data + "]");
+        }
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool SetProcessDPIAware();
     }
 }
