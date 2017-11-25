f5f98c757fefbd5b76c3c651c5d32268f949b0f9
Fronbasal <danielmalik@outlook.de>
2017-11-25 17:37:15 +0100

    Fixed one dimensional array complications

--- Program.cs
+++ Program.cs
@@ -26,65 +26,60 @@ namespace dialektor
 
             var tuple = getData();
 
-            // Split data to training and testing data
-            double[][] data_train_x = new double[(int)(tuple.mfcc_inputs.Length * 0.66)][];
-            double[][] data_test_x = new double[(int)(tuple.mfcc_inputs.Length * 0.33)][];
+            double[][] data_train_x = new double[(int) (tuple.mfcc_inputs.Length * 0.66)][];
+            double[][] data_test_x = new double[(int) (tuple.mfcc_inputs.Length * 0.33)][];
 
-            bool[] data_train_y = new bool[(int)(tuple.classes.Length * 0.66)];
-            bool[] data_test_y = new bool[(int)(tuple.classes.Length * 0.33)];
+            bool[] data_train_y = new bool[(int) (tuple.classes.Length * 0.66)];
+            bool[] data_test_y = new bool[(int) (tuple.classes.Length * 0.33)];
 
-            // Copy data to arrays
-            Array.Copy(tuple.mfcc_inputs, 0, data_train_x, 0, (int)(tuple.mfcc_inputs.Length * 0.66));
-            Array.Copy(tuple.mfcc_inputs, 0, data_test_x, (int)(tuple.mfcc_inputs.Length * 0.66), tuple.mfcc_inputs.Length);
+            Array.Copy(tuple.mfcc_inputs, 0, data_train_x, 0, (int) (tuple.mfcc_inputs.Length * 0.66));
+            Array.Copy(tuple.mfcc_inputs, 0, data_test_x, (int) (tuple.mfcc_inputs.Length * 0.66),
+                tuple.mfcc_inputs.Length);
 
-            Array.Copy(tuple.classes, 0, data_train_y, 0, (int)(tuple.classes.Length * 0.66));
-            Array.Copy(tuple.classes, 0, data_test_y, (int)(tuple.classes.Length * 0.66), tuple.classes.Length);
-            
-            // Plot dimensions
+            Array.Copy(tuple.classes, 0, data_train_y, 0, (int) (tuple.classes.Length * 0.66));
+            Array.Copy(tuple.classes, 0, data_test_y, (int) (tuple.classes.Length * 0.66), tuple.classes.Length);
+
             Console.WriteLine("Training dimension: (" + data_train_x.Length + ", " + data_train_y.Length + ")");
             Console.WriteLine("Test dimension: (" + data_test_x.Length + ", " + data_test_y.Length + ")");
 
-            //Train & test
             DeepBeliefNetwork network = train(ref data_train_x, ref data_train_y);
             double[][] prediction_proba = predict(network, data_test_x);
 
             double similarity = 0;
             for (int i = 0; i < prediction_proba.Length; i++)
             {
-                //Class is 1 if propablity for class 1 is greater than 0.5
                 bool class_predicted = prediction_proba[i][0] > 0.5;
                 bool class_real = data_test_y[i];
                 similarity += Convert.ToInt32(class_predicted == class_real);
             }
             similarity /= prediction_proba.Length;
-            
+
             Console.WriteLine("Similarity: " + similarity + "%");
 
-            while (true) { Console.ReadKey(true); }
+            while (true)
+            {
+                Console.ReadKey(true);
+            }
         }
 
         public static unsafe DeepBeliefNetwork train(ref double[][] inputs, ref bool[] outputs_classes)
         {
             double[][] outputs = (from output in outputs_classes
-                               select output == true ? new double[] { 1, 0 } : new double[] { 0, 1 }).ToArray();
+                select output == true ? new double[] {1, 0} : new double[] {0, 1}).ToArray();
 
             DeepBeliefNetwork network = new DeepBeliefNetwork(inputsCount: inputs.Length,
-                                                hiddenNeurons: new int[] { 250, 200, 200, 25 }); // 隠れ層と出力層の次元
-            // DNNの学習アルゴリズムの生成
+                hiddenNeurons: new int[] {250, 200, 200, 25});
             var teacher = new DeepNeuralNetworkLearning(network)
             {
                 Algorithm = (ann, i) => new ParallelResilientBackpropagationLearning(ann),
                 LayerIndex = network.Machines.Count - 1
             };
 
-            // 5000回学習
             var layerData = teacher.GetLayerInput(inputs);
             for (int i = 0; i < 5000; i++)
             {
                 teacher.RunEpoch(layerData, outputs);
             }
-
-            // 重みの更新
             network.UpdateVisibleWeights();
             return network;
         }
@@ -114,7 +109,8 @@ namespace dialektor
             Signal signal = decoder.Decode();
             MelFrequencyCepstrumCoefficient mfcc = new MelFrequencyCepstrumCoefficient();
             MelFrequencyCepstrumCoefficientDescriptor[] ra = mfcc.Transform(signal).ToArray();
-            return ra;
+            var query = from x in ra select x.Descriptor.Average();
+            return query.ToArray();
         }
 
         [System.Runtime.InteropServices.DllImport("user32.dll")]
